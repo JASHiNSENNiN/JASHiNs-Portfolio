49066c61355d384b748c0e6d023e60547d61700e
JASHiNSENNiN <jacercinense21@gmail.com>
2023-09-26 00:04:48 +0800

    website overhaul into a personal portfolio

---
+++ js/terminal.js
@@ -0,0 +1,53 @@
+const terminalData = [
+    { text: "whoami   ", additionalMessage: "Name: Josh V. Cinense<br>    Age: 20 <br><br>    Creates things with the power of creativity and efficiency" },
+  ];
+  const terminalElements = document.getElementsByClassName("terminal");
+  
+  function writeText() {
+    let currentIndex = 0;
+    function typeText() {
+      const targetElement = terminalElements[currentIndex];
+      const terminal = terminalData[currentIndex];
+      const text = terminal.text;
+      let index = 0;
+      const initialText = targetElement.textContent;
+  
+      const intervalId = setInterval(() => {
+        targetElement.textContent = initialText + text.substring(0, index + 1);
+        index++;
+  
+        if (index === text.length) {
+          clearInterval(intervalId);
+  
+          const extraMessageElement = document.createElement("div");
+          extraMessageElement.innerHTML = terminal.additionalMessage;
+          extraMessageElement.classList.add("extra-message");
+          targetElement.parentNode.insertBefore(
+            extraMessageElement,
+            targetElement.nextSibling
+          );
+  
+          const cursorElement = document.createElement("span");
+          cursorElement.classList.add("cursor");
+          cursorElement.textContent = "|"; // Set the cursor text or symbol
+  
+          // Append the cursor only if the additional message is not empty
+          if (terminal.additionalMessage.trim() !== "") {
+            extraMessageElement.appendChild(cursorElement);
+          }
+  
+          currentIndex++; // Move to the next terminal
+  
+          if (currentIndex < terminalElements.length) {
+            // If there are more terminals, type the next one with a slight delay
+            setTimeout(typeText, 500);
+          }
+        }
+      }, 200); // Adjust the interval duration (in milliseconds) to control the speed of typing
+    }
+  
+    // Start typing the first terminal
+    typeText();
+  }
+  
+  writeText();
